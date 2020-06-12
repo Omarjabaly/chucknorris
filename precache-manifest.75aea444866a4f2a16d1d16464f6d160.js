2b588c3a1b3159d712b4496da016a6ab08797c9f
Omarjabaly <60774903+Omarjabaly@users.noreply.github.com>
2020-06-12 19:04:32 +0300

    Updates

---
+++ precache-manifest.75aea444866a4f2a16d1d16464f6d160.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "378e5e07539975921163e1366662a259",
+    "url": "/chucknorris/index.html"
+  },
+  {
+    "revision": "ae0755871b7e4398e02f",
+    "url": "/chucknorris/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "917c184ad1e127c8610d",
+    "url": "/chucknorris/static/css/main.65a00982.chunk.css"
+  },
+  {
+    "revision": "ae0755871b7e4398e02f",
+    "url": "/chucknorris/static/js/2.83686f8a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/chucknorris/static/js/2.83686f8a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "917c184ad1e127c8610d",
+    "url": "/chucknorris/static/js/main.ac2966de.chunk.js"
+  },
+  {
+    "revision": "f21078ba13f13e3c618d",
+    "url": "/chucknorris/static/js/runtime-main.14e2fa2e.js"
+  },
+  {
+    "revision": "3becd5757959f996a3175dad96690418",
+    "url": "/chucknorris/static/media/ComicHelvetic_Medium.3becd575.otf"
+  }
+]);
